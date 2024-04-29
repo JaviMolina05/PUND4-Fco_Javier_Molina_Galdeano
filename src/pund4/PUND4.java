e6a6f0a6c7c184511f6303f9dac98c7f0d0b7752
javi <javi@DESKTOP-18T9E1F>
2024-04-29 11:01:03 +0200

    refactorizado

---
+++ src/pund4/PUND4.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pund4;
+
+/**
+ *
+ * @author javie
+ */
+public class PUND4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Apuesta laApuesta;
+        int mi_dinero;
+
+        laApuesta = new Apuesta(1000, 4, 2);
+        operativa_apuesta(laApuesta, 0);
+    }
+
+    private static void operativa_apuesta(Apuesta laApuesta, int dinero) {
+        int mi_dinero;
+        try {
+            System.out.println("Apostando...");
+            laApuesta.apostar(25);
+        } catch (Exception e) {
+            System.out.println("Fallo al realizar la Apuesta");
+        }
+        try {
+            System.out.println("Intento cobrar apuesta segun el resultado del partido");
+            laApuesta.cobrar_apuesta(2, 3);
+        } catch (Exception e) {
+            System.out.println("Fallo al cobrar la apuesta");
+        }
+        mi_dinero = laApuesta.getDinero_disp();
+        System.out.println("El dinero que tengo tras las apuestas es " + mi_dinero);
+    }
+    
+}
